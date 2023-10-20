8219a7c16d4d6c91a81eefccf36eec11029957ca
MartinezFabian <FabianMartinezDev@gmail.com>
2023-10-19 21:21:51 -0300

    Feat: Add floating action button in HomePage.jsx

--- src/LifeDiary/pages/HomePage.jsx
+++ src/LifeDiary/pages/HomePage.jsx
@@ -1,11 +1,22 @@
+import { Fab } from '@mui/material';
 import { LifeDiaryLayout } from '../layout/LifeDiaryLayout';
-import { NoteView } from '../views/NoteView';
+import { Add } from '@mui/icons-material';
 import { NothingSelectedView } from '../views/NothingSelectedView';
+import { NoteView } from '../views/NoteView';
 
 export const HomePage = () => {
   return (
     <LifeDiaryLayout>
-      <NoteView></NoteView>
+      <NothingSelectedView></NothingSelectedView>
+
+      <Fab
+        size="large"
+        color="secondary"
+        aria-label="add"
+        sx={{ position: 'fixed', right: 50, bottom: 50 }}
+      >
+        <Add />
+      </Fab>
     </LifeDiaryLayout>
   );
 };

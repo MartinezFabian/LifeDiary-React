e30e54d50ede085d43f526eb6e810ce98bd98738
MartinezFabian <FabianMartinezDev@gmail.com>
2023-11-08 18:06:05 -0300

    Feat: Set the active note when clicked

--- src/LifeDiary/components/SideBarList.jsx
+++ src/LifeDiary/components/SideBarList.jsx
@@ -1,15 +1,17 @@
 import TurnedInNot from '@mui/icons-material/TurnedInNot';
 import { Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { setActiveNote } from '../../store/slices/lifeDiary/lifeDiarySlice';
 
 export const SideBarList = () => {
   const { notes } = useSelector((state) => state.lifeDiary);
+  const dispatch = useDispatch();
 
   return (
     <List>
       {notes.map((note) => (
         <ListItem key={note.id} disablePadding>
-          <ListItemButton>
+          <ListItemButton onClick={() => dispatch(setActiveNote(note))}>
             <ListItemIcon>{<TurnedInNot></TurnedInNot>}</ListItemIcon>
             <Grid container overflow="hidden">
               <ListItemText

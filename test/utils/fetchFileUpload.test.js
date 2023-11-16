093a42df8360c3b89792fe15a33033a92bda1354
MartinezFabian <FabianMartinezDev@gmail.com>
2023-11-16 20:49:58 -0300

    test [fetchFileUpload.js]: delete cloudinary test image

--- test/utils/fetchFileUpload.test.js
+++ test/utils/fetchFileUpload.test.js
@@ -1,8 +1,22 @@
 /* eslint-disable no-undef */
+
+import { v2 as cloudinary } from 'cloudinary';
 import { fetchFileUpload } from '../../src/utils/fetchFileUpload';
+import { getEnvironments } from '../../src/utils/getEnvironments';
+
+const { VITE_CLOUDINARY_CLOUD_NAME, VITE_CLOUDINARY_API_KEY, VITE_CLOUDINARY_API_SECRET } =
+  getEnvironments();
+
+cloudinary.config({
+  cloud_name: VITE_CLOUDINARY_CLOUD_NAME,
+  api_key: VITE_CLOUDINARY_API_KEY,
+  api_secret: VITE_CLOUDINARY_API_SECRET,
+  secure: true,
+});
 
 describe('tests in fetchFileUpload.js', () => {
   test('must upload the file correctly to cloudinary', async () => {
+    // convertir una URL de imagen en un archivo para poder probar 'fetchFileUpload(file)'
     const urlImage =
       'https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png';
     const resp = await fetch(urlImage);
@@ -13,5 +27,14 @@ describe('tests in fetchFileUpload.js', () => {
 
     expect(secureUrl).toContain('https://res.cloudinary.com/');
     expect(secureUrl).toContain('/image/upload/');
+
+    // Obtener el ID de la imagen desde la URL
+    const segments = secureUrl.split('/');
+    const imageId = segments[segments.length - 1].replace('.png', '');
+
+    // eliminar la imagen de prueba de cloudinary
+    const cloudinaryResp = await cloudinary.api.delete_resources(['life_diary/' + imageId]);
+
+    console.log(cloudinaryResp);
   });
 });

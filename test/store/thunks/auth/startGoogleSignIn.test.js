c31b3ea865ad3c6f51b6557994050bad35721951
MartinezFabian <FabianMartinezDev@gmail.com>
2023-11-18 19:24:14 -0300

    test: Add tests in startGoogleSignIn.js

---
+++ test/store/thunks/auth/startGoogleSignIn.test.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-undef */
+import { signInWithGoogle } from '../../../../src/firebase/providers';
+import { checkingCredentials, login, logout } from '../../../../src/store/slices/auth/authSlice';
+import { startGoogleSignIn } from '../../../../src/store/thunks/auth/startGoogleSignIn';
+
+import { testUser } from '../../../fixtures/authFixtures';
+
+jest.mock('../../../../src/firebase/providers');
+
+describe('tests in startGoogleSignIn.js', () => {
+  test('must call checkingCredentials and login', async () => {
+    const dispatch = jest.fn();
+
+    const loginData = {
+      ok: true,
+      ...testUser,
+    };
+
+    // mock
+    await signInWithGoogle.mockResolvedValue(loginData);
+
+    // thunk
+    await startGoogleSignIn()(dispatch);
+
+    expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+    expect(dispatch).toHaveBeenCalledWith(login(loginData));
+  });
+
+  test('must call checkingCredentials and logout with errorMessage', async () => {
+    const dispatch = jest.fn();
+
+    const loginData = {
+      ok: false,
+      errorMessage:
+        'An account already exists with the same email address but different sign-in credentials.',
+    };
+
+    // mock
+    await signInWithGoogle.mockResolvedValue(loginData);
+
+    // thunk
+    await startGoogleSignIn()(dispatch);
+
+    expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+    expect(dispatch).toHaveBeenCalledWith(logout(loginData.errorMessage));
+  });
+});

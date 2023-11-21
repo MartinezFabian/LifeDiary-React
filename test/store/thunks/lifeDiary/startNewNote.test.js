e0dd30e38192d51c1fc688547d57f4f22ec47a70
MartinezFabian <FabianMartinezDev@gmail.com>
2023-11-21 18:18:51 -0300

    test: Add tests in startNewNote.js

---
+++ test/store/thunks/lifeDiary/startNewNote.test.js
@@ -0,0 +1,55 @@
+import { collection, deleteDoc, getDocs } from 'firebase/firestore/lite';
+import { firestoreDB } from '../../../../src/firebase/config';
+import {
+  addNewEmptyNote,
+  savingNewNote,
+  setActiveNote,
+} from '../../../../src/store/slices/lifeDiary/lifeDiarySlice';
+import { startNewNote } from '../../../../src/store/thunks/lifeDiary/startNewNote';
+
+/* eslint-disable no-undef */
+describe('tests in startNewNote.js', () => {
+  const dispatch = jest.fn();
+  const getState = jest.fn();
+
+  beforeEach(() => jest.clearAllMocks());
+
+  test('must create a new empty note', async () => {
+    getState.mockReturnValue({ auth: { uid: 'TEST_UID' } });
+
+    await startNewNote()(dispatch, getState);
+
+    expect(dispatch).toHaveBeenCalledWith(savingNewNote());
+
+    expect(dispatch).toHaveBeenCalledWith(
+      addNewEmptyNote({
+        id: expect.any(String),
+        title: '',
+        body: '',
+        date: expect.any(Number),
+        imagesUrls: [],
+      })
+    );
+
+    expect(dispatch).toHaveBeenCalledWith(
+      setActiveNote({
+        id: expect.any(String),
+        title: '',
+        body: '',
+        date: expect.any(Number),
+        imagesUrls: [],
+      })
+    );
+
+    // delete firebase test data
+
+    const colecctionRef = collection(firestoreDB, `TEST_UID/lifeDiary/notes`);
+    const docs = await getDocs(colecctionRef);
+
+    const deletePromises = [];
+
+    docs.forEach((doc) => deletePromises.push(deleteDoc(doc.ref)));
+
+    await Promise.all(deletePromises);
+  }, 10000);
+});
